fac06ad5ebc69c8bee6ac1991e4e1e206518f0d6
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-09 13:58:38 -0700

    updating dns

--- dns.tf
+++ dns.tf
@@ -1,5 +1,4 @@
 resource "aws_route53_record" "service" {
-  count = "${var.set_dns ? 1 : 0}"
   zone_id = "${data.consul_keys.dns.var.dns_zone}"
   name    = "${var.service_name}.${data.consul_keys.dns.var.dns_name}"
   type    = "CNAME"
