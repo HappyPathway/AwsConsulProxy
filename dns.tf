fa651a8b8ec004893eb0c6bc0e7948923faa19b2
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-10 16:01:18 -0700

    adding proxy_name and dns.tf

---
+++ dns.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "service" {
+  count   = "${length(module.aws.instance_ips)}"
+  zone_id = "${data.consul_keys.dns.var.dns_zone}"
+  name    = "${var.proxy_name}.$${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${module.aws.instance_ips}"]
+}
--- playbooks/proxy_bootstrap.sh
+++ playbooks/proxy_bootstrap.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install -r /tmp/playbooks/requirements.txt
-sudo ansible-playbook /tmp/playbooks/proxy_install.yaml -e env=$(cat /tmp/app.env) -e service_name=$(cat /tmp/app.service) -e app_name=$(cat /tmp/app.name)
+sudo ansible-playbook /tmp/playbooks/proxy_install.yaml \
+-e env=$(cat /tmp/app.env) \
+-e service_name=$(cat /tmp/app.service) \
+-e app_name=$(cat /tmp/app.name) \
+-e proxy_name=$(cat /tmp/app.proxy)
+
 sudo rm -rf /tmp/playbooks
