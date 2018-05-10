55d5653ff6c30194af85cbc43f9a479ae96cb3b4
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-10 16:05:48 -0700

    adding proxy_name

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "service" {
   count   = "${length(module.aws.instance_ips)}"
   zone_id = "${data.consul_keys.dns.var.dns_zone}"
-  name    = "${var.proxy_name}.$${var.domain}"
+  name    = "${var.proxy_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${module.aws.instance_ips}"]
