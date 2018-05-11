257d6727a262526558f7fe85b89f583dae86be08
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-11 11:01:06 -0700

    fixing syntax error

--- dns.tf
+++ dns.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "proxy_service" {
   depends_on = [
     "module.aws"
   ]
-  count   = "${var.count)}"
+  count   = "${var.count}"
   zone_id = "${data.consul_keys.dns.var.dns_zone}"
   name    = "${var.proxy_name}"
   type    = "A"
