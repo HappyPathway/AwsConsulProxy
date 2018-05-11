90e55f8428fad4f76ee8e7dfc24052d02458e7f8
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-10 22:34:56 -0700

    fixing dns.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "dns_nodes" {
-    value = "${aws_route53_record.service_nodes.fqdn}"
-}
-
 output "dns" {
-    value = "${aws_route53_record.service.fqdn}"
+    value = "${aws_route53_record.proxy_service.fqdn}"
 }
